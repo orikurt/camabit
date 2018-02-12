1cedac4ec35afa7456c7eeb115f85a0ccc575b0b
orikurt <orikball@gmail.com>
2018-02-12 11:10:16 +0200

    use gen.Task to fetch coins asyncly

--- lib/coin_importer.py
+++ lib/coin_importer.py
@@ -1,5 +1,6 @@
 import json
 from tornado.httpclient import AsyncHTTPClient
+from tornado.gen import Task
 
 API_URL = "https://api.coinmarketcap.com/v1/"
 http_client = AsyncHTTPClient()
@@ -8,7 +9,7 @@ async def all_coins():
     url = "{0}ticker/?limit=10&convert=ILS".format(API_URL)
     try:
         print("fetching")
-        coins = await http_client.fetch(url)
+        coins = await Task(http_client.fetch, url)
         print("fetched {} bytes".format(len(coins.body)))
         coins = coins.body.decode("utf8")
         return json.loads(coins)
