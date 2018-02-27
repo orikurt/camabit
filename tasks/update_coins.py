f5c460b4f5243c35111e71ee3a1fb62a20bca724
orikurt <orikball@gmail.com>
2018-02-27 13:58:31 +0200

    use divs to build coin table comp

--- tasks/update_coins.py
+++ tasks/update_coins.py
@@ -5,6 +5,9 @@ from camabit.lib import coin_importer
 
 async def run():
     all_coins = await coin_importer.all_coins() 
+    if not all_coins:
+        print("No coins received on import")
+        return
     print("running coin update: All coins {}".format(len(all_coins)))
     for coin_data in all_coins:
         coin = await Coin.first_or_create(coin_data)
