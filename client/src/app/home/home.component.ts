95dd323bd06f14b0639ad14616e21558398c60a9
orikurt <orikball@gmail.com>
2018-03-07 18:00:20 +0200

    better layout, bring back spinners

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { CoinService } from '../coin.service';
-import { Coin } from '../coin';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger('toggleMenu', [
       state('down', style({
@@ -21,25 +18,13 @@ import { Coin } from '../coin';
   ]      
 })
 export class HomeComponent implements OnInit {
-  coins: Coin[];
-  disposable;
   metaState;
 
-  constructor(private coinService: CoinService, private cdr: ChangeDetectorRef) {
-    this.coins = [];
+  constructor() {
     this.metaState = "down";
   }
 
-  ngOnInit() {
-    this.cdr.detach();
-    this.disposable = this.coinService.coins.subscribe(coins => {
-      this.coins = [this.coinService.hot()];
-      this.cdr.detectChanges();
-      if(this.disposable && this.coins.length){
-        this.disposable.unsubscribe();
-      }
-    });
-  }
+  ngOnInit() {}
 
   toggleMeta(){
     this.metaState = this.metaState == "down" ? "up" : "down";
