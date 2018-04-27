19c73a53addfedca0e1fd60970b6443c3e6de528
orikurt <orikball@gmail.com>
2018-04-27 18:26:24 +0300

    move coins table to coins page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoinService } from './coin.service';
 import { CoinsResolve } from './resolvers/coins.resolver';
-import { AllCoinsComponent } from './all-coins/all-coins.component';
+import { CoinsComponent } from './coins/coins.component';
 import { CoinPageComponent } from './coin-page/coin-page.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "coins", component: AllCoinsComponent, resolve: {coins: CoinsResolve} },
+  { path: "coins", component: CoinsComponent},
   { path: "coins/:id", component: CoinPageComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
